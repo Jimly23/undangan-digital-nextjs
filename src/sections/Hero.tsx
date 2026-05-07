acc5b0529412fc65e5710350ebc316aac148bea6
Jimly23 <jimlyasidqi692@gmail.com>
2026-05-07 23:37:29 +0700

    Update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,11 +4,11 @@ import { ElementBawah } from "../elements/ElementBawah";
 
 export default function Hero() {
   return (
-    <div id="hero" className="max-h-screen relative overflow-hidden">
-      <Image src="/assets/themes/royal-garden/bg-gunung.webp" alt="bg-gunung" width={1280} height={720} className="w-full h-screen" />
+    <div id="hero" className="h-screen w-full relative overflow-hidden bg-blue-400">
+      {/* <Image src="/assets/themes/royal-garden/bg-gunung.webp" alt="bg-gunung" width={1280} height={720} className="w-full h-screen" />
       <Image src="/assets/themes/royal-garden/daunatastengah.webp" alt="ornamen-atas" width={1280} height={720} className="absolute top-0 left-0 right-0" />
       <ElementAtas />
-      <ElementBawah />
+      <ElementBawah /> */}
 
       <div className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2">
         <h5 data-aos="zoom-in-up" className="text-white text-center text-lg mb-2">THE WEDDING OF</h5>
