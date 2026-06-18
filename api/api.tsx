b57a54dcefe74123a6aba440c2301afa218cf52d
Jimly23 <jimlyasidqi692@gmail.com>
2026-06-18 20:47:04 +0700

    Update

--- api/api.tsx
+++ api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
-const BASE_URL = `${API_URL}/api/undangan`;
+const BASE_URL = `${API_URL}/undangan`;
 const INVITATION_URL = `${API_URL}/invitation`;
 
 export const api = {
